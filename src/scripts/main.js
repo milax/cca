a3eb7e76a3d76923f0b4c64ae063d83799b8882e
Eugene Kuzmin <eugene.kuzmin@creuna.com>
2015-01-26 19:05:55 +0200

    Scrolling

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,62 @@
 'use strict';
 
+var CCA = {} || CCA;
+
 (function($){
-  console.log($);
+  
+  CCA.Scroll = function() {
+
+    var $go_links = $('.go-next, .go-top');
+
+    var attachEvents = function() {
+      $go_links.click(go);
+    };
+
+    var go = function() {
+      var $self = $(this);
+
+      var $parent_site_section,
+        $parent_site,
+        $next_box;
+
+      if($self.hasClass('go-top')) {
+        scrollToBox(0);
+      }
+      else if($self.hasClass('go-next')) {
+        $parent_site_section = $self.closest('.site--section');
+        $parent_site         = $parent_site_section.closest('.site');
+        $next_box            = $parent_site_section.next('.site--section');
+
+        $next_box = $next_box.length ? $next_box : $parent_site.next('.site');
+
+        if($next_box.length) {
+          scrollToBox($next_box);
+        }
+      }
+
+
+      function scrollToBox($box) {
+        $('html, body').animate({
+          scrollTop: $box ? $box.offset().top : 0
+        }, 500);
+      }
+
+      return false;
+    };
+
+    var init = function() {
+      attachEvents();
+    };
+
+    return {
+      init: init
+    };
+
+  };
+
+  $(function(){
+    var scroll = new CCA.Scroll();
+    scroll.init();
+  });
+
 })(jQuery);
