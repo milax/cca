06cdb7a816b552b3cbc73d892375aeff28b56c21
Eugene Kuzmin <eugene.kuzmin@creuna.com>
2015-01-28 20:34:50 +0200

    Work with prismic api

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -62,9 +62,95 @@ var CCA = {} || CCA;
 
   };
 
+  CCA.RenderPage = function() {
+
+    var getSiteInfo = function(site) {
+      var s = {};
+
+      s.url       = site.slug;
+      s.title     = site.fragments['article.title'].value[0].text;
+
+      if(site.fragments['article.cuff']) {
+        s.cuff = site.fragments['article.cuff'].value[0].text;
+      }
+
+      if(site.fragments['article.illustration']) {
+        s.image = {
+          url: site.fragments['article.illustration'].value.main.url
+        };
+      }
+
+      if(site.fragments['article.content']) {
+        s.content = site.fragments['article.content'].value;
+      }
+
+      return s;
+    };
+
+    var init = function() {
+
+      var hash = window.location.hash.substring(1);
+
+      Helpers.withPrismic(function(ctx) {
+
+        ctx.api.form('everything').ref(ctx.ref).submit(function(err, sites) {
+          
+          var received_sites = sites.results;
+          var sites_to_render = [];
+          var i, len;
+
+          if (err) {
+            Configuration.onPrismicError(err);
+            return;
+          }
+
+          if(hash) {
+            for(i = 0, len = received_sites.length; i < len; ++i) {
+              if(received_sites[i].slug === hash) {
+                sites_to_render.push(getSiteInfo(received_sites[i]));
+                break;
+              }
+            }
+          }
+
+          if(!hash || !sites_to_render.length) {
+            for(i = 0, len = received_sites.length; i < len; ++i) {
+              sites_to_render.push(getSiteInfo(received_sites[i]));
+            }
+          }
+
+
+          // Feed the template
+          $('#sites').render({
+              sites: sites_to_render,
+              hash: hash
+            },
+            function() {
+              $('[data-forcereload=true]').click(function(){
+                window.location = $(this).attr('href');
+                window.location.reload();
+              });
+            }
+          );
+
+        });
+
+      });
+    };
+
+    return {
+      init: init
+    };
+
+  };
+
   $(function(){
     var scroll = new CCA.Scroll();
     scroll.init();
+
+    var render = new CCA.RenderPage();
+    render.init();
+
   });
 
 })(jQuery);
